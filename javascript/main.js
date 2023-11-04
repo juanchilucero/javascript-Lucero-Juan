6f858d10fee08ea2376c8b16050dbfcd56aff218
Juan Ignacio Lucero Vazquez <juanchi98lucero@gmail.com>
2023-11-04 15:31:37 -0300

    creando juego

---
+++ javascript/main.js
@@ -0,0 +1,63 @@
+
+
+// Funcion eleccion del jugador
+function obtenerEleccionJugador() 
+{
+    let eleccion = prompt("Elige: piedra, papel o tijeras").toLowerCase();
+    while (eleccion !== "piedra" && eleccion !== "papel" && eleccion !== "tijeras") 
+    {
+        alert("Eleccion no valida. Intentalo de nuevo.");
+        eleccion = prompt("Elige: piedra, papel o tijeras").toLowerCase();
+    }
+    return eleccion;
+}
+
+  // Funcion computadora
+function obtenerEleccionComputadora() 
+{
+    const opciones = ["piedra", "papel", "tijeras"];
+    const eleccionAleatoria = Math.floor(Math.random() * 3);
+    return opciones[eleccionAleatoria];
+}
+
+  // Funcion resultado
+function determinarResultado(eleccionJugador, eleccionComputadora) 
+{
+    if (eleccionJugador === eleccionComputadora) 
+    {
+    return "¡Es un empate!";
+    } else if (
+        (eleccionJugador === "piedra" && eleccionComputadora === "tijeras") ||
+        (eleccionJugador === "papel" && eleccionComputadora === "piedra") ||
+        (eleccionJugador === "tijeras" && eleccionComputadora === "papel")
+    ) {
+        return "¡Ganaste!";
+    } else {
+        return "Lo siento, Perdiste. ¡Intentalo de nuevo!";
+    }
+}
+
+  // Funcion ejecutar juego
+function jugarPiedraPapelTijeras() {
+    let continuarJugando = true;
+
+    while (continuarJugando) {
+        const eleccionJugador = obtenerEleccionJugador();
+        const eleccionComputadora = obtenerEleccionComputadora();
+        const resultado = determinarResultado(eleccionJugador, eleccionComputadora);
+        alert(`Tu eleccion: ${eleccionJugador}\nEleccion de la computadora: ${eleccionComputadora}\n${resultado}`);
+
+        const respuesta = prompt("¿Quieres jugar de nuevo? (Si/No)").toLowerCase();
+
+        if (respuesta === "no") {
+            continuarJugando = false;
+            alert("Gracias por jugar");
+        } else if (respuesta !== "si") {
+            alert("Respuesta no valida. Por favor, ingresa 'Si' o 'No'.");
+        }
+    }
+}
+
+
+  // Iniciar funcion juego
+jugarPiedraPapelTijeras();
